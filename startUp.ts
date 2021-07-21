850c272ded6bebf13e19d4b6198e14045a37a83d
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2021-07-20 22:18:40 -0300

    routes already setup

--- startUp.ts
+++ startUp.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
+
 import Db from './infra/db';
+import NewsController from './controller/newsController';
 
 
 class StartUp {
@@ -10,11 +12,11 @@ class StartUp {
      this.app = express();
      this._db = new Db();
      this._db.createConnection();
-     this.middler();
+     this.middleware();
      this.routes();
    }
 
-   middler(){
+   middleware(){
       this.app.use(express.json());
       this.app.use(express.urlencoded({extended: false}));
    }
@@ -23,6 +25,14 @@ class StartUp {
      this.app.route('/teste').get((req, res) => {
        res.send({ versao: '0.0.1' });
      });
+
+     this.app.route('/api/v1/news').get(NewsController.get);
+     this.app.route('/api/v1/news:id').get(NewsController.getById);
+     this.app.route('/api/v1/news/').post(NewsController.create);
+     this.app.route('/api/v1/news/:id').put(NewsController.update);
+     this.app.route('/api/v1/news/').delete(NewsController.delete);
+
+
    }
 }
 
